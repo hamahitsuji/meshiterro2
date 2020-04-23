330afdd3ec7dab5d129370ec36cedf6291f37370
vagrant <vagrant@localhost.localdomain>
2020-04-23 16:12:11 +0000

    [Add] コメント・いいね機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,9 +11,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1f8082a6139f03b9f63da96a68c8538ea4df29779c0280c517f98754275c1806ee5148c0530da89e307f8e8209bc0e81118f24e8a98705b68dcb6ef279aaefa2'
